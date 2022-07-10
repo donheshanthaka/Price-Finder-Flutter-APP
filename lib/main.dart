894e254808448bafa8b05f1639b4fd75b4475f32
Don Heshanthaka <heshanthaka007@gmail.com>
2022-07-10 21:46:55 +0530

    Implement functionality to save the image to working directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,15 +34,32 @@ class _HomePageState extends State<HomePage> {
   File? _image;
 
   Future getImage() async {
-    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image == null) return;
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
 
-    final imageTemp = File(image.path);
+      // final imageTemp = File(image.path);
+      final imageSaved = await saveImage(image.path);
+      // debugPrint('Image Location >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+      // debugPrint(image.path);
 
-    setState(() {
-      _image = imageTemp;
-    });
+      setState(() {
+        _image = imageSaved;
+      });
+    } on PlatformException catch (e) {
+      debugPrint('Failed to display image: $e');
+    }
   }
+
+Future<File> saveImage(String imagePath) async {
+  final direcotry = await getApplicationDocumentsDirectory();
+  final name = basename(imagePath);
+  final image = File('${direcotry.path}/$name');
+  debugPrint('direcotry path >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+  debugPrint('${direcotry.path}/$name');
+
+  return File(imagePath).copy(image.path);
+}
 
   @override
   Widget build(BuildContext context) {
