fe3b05978e5cd46cedc5e3e4842ec49825c0877d
Don Heshanthaka <heshanthaka007@gmail.com>
2022-07-28 23:58:22 +0530

    Change predictImage to call getPrice and return a list of information containing both model and price

--- lib/predict_image.dart
+++ lib/predict_image.dart
@@ -1,27 +1,38 @@
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:price_finder/failure.dart';
 
-Future<String> predictImage(File file) async {
+Future<List<String>> predictImage(File file) async {
   //String url = "https://get-prediction-hezxtblxwq-el.a.run.app";
   String localPredUrl = "http://192.168.1.101:8000/"; // // use the current ip address by running ipconfig at the time of debugging using emulator
   
   //print('Image path >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
   //print(file.path);
+
+  List<String> info = [];
+
   var request = http.MultipartRequest("POST", Uri.parse(localPredUrl));
 
   var multipartFile = await http.MultipartFile.fromPath('imageFile', file.path);
 
   request.files.add(multipartFile);
 
-  var response = await request.send();
-  if (response.statusCode == 200) {
-    var responseData = await response.stream.toBytes();
-    var prediction = String.fromCharCodes(responseData);
-    return prediction;
-  } else {
-    throw Exception("Falied to indentify image");
+  try{
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      var responseData = await response.stream.toBytes();
+      var vehicleName = String.fromCharCodes(responseData);
+      info.add(vehicleName);
+      info.add(await getPrice(vehicleName));
+      return info;
+    } else {
+      throw Exception("Falied to indentify image");
+    }
+  } on SocketException {
+    throw Failure("Error: Cannot connect to server!");
   }
+  
 }
 
 Future<String> getPrice(String vehicleName) async{
