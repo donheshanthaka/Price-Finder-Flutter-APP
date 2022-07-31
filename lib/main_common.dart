b4dc1644fc7ac230f5b77626162f8ba495f7313b
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-01 05:15:10 +0530

    Create main_common.dart

---
+++ lib/main_common.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:price_finder/config_reader.dart';
+import 'package:price_finder/app_widget.dart';
+
+
+Future<void> mainCommon(String env) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ConfigReader.initialize(env);
+
+  runApp(const MyApp());
+}
