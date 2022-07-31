4e47d614841b8b43b5b3351a3d26ec38b187a9b3
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-01 05:16:59 +0530

    Rename main.dart to app_widget.dart

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -12,9 +12,9 @@ import 'package:price_finder/image_retrieval.dart';
 import 'package:price_finder/failure.dart';
 import 'package:price_finder/models/vehicle.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:price_finder/main.dart';
+import 'package:price_finder/app_widget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
