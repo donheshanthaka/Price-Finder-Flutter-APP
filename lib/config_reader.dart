49f1bbe50b6648bc568277558097cf69fe5415a8
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-01 04:13:05 +0530

    Create config_reader.dart that holds the ConfigReader class which reads the configuration file based on environment

---
+++ lib/config_reader.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:price_finder/environment.dart';
+
+abstract class ConfigReader {
+  static late Map<String, dynamic> _config;
+
+  static Future<void> initialize(String env) async {
+    late String path;
+    switch (env){
+      case Environment.dev:
+        path = 'config/app_config_dev.json';
+        break;
+      case Environment.prod:
+        path = 'config/app_config_prod.json';
+        break;
+    }
+    final configString = await rootBundle.loadString(path);
+    _config = json.decode(configString) as Map<String, dynamic>;
+  }
+
+  static String getApiUrl() {
+    return _config['API_URL'] as String;
+  }
+}
