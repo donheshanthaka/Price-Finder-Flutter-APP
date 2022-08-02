93f1ba82ffc1281e5239becc536985d51a8fa09a
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-02 23:59:29 +0530

    Create Image model class

---
+++ lib/features/select_image/models/image_model.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Image {
+  static Future<File?> getImage(String imageSource) async {
+    late ImageSource source;
+    if (imageSource == 'camera'){
+      source = ImageSource.camera;
+    } else if (imageSource == 'gallery'){
+      source = ImageSource.gallery;
+    }
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return null;
+      //final savedImage = await _saveImage(image.path);
+
+      //await predictImage(imageSaved);
+      //return savedImage;
+      return File(image.path);
+    } on PlatformException catch (e) {
+      print('Failed to display image: $e');
+    }
+    return null;
+  }
+}
