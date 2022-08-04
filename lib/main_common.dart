bdc501be9a7905c912a11df5fbf4d7296894fc34
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-04 09:12:12 +0530

    Rename app_widget.dart to app.dart

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:price_finder/config_reader.dart';
-import 'package:price_finder/app_widget.dart';
+import 'package:price_finder/app.dart';
 
 
 Future<void> mainCommon(String env) async {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:price_finder/app_widget.dart';
+import 'package:price_finder/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
