3dce01ad7b030a3b5081d62021c6c240a3f81a59
Don Heshanthaka <heshanthaka007@gmail.com>
2022-09-26 09:06:43 +0530

    Add background color and image re-shape in vehicle page

--- lib/features/search_image/view/vehicle_page_view.dart
+++ lib/features/search_image/view/vehicle_page_view.dart
@@ -48,54 +48,95 @@ class _VehiclePageState extends State<VehiclePage> {
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.waiting:
-                return SearchView(image: widget.image,);
+                return SearchView(
+                  image: widget.image,
+                );
               case ConnectionState.done:
               default:
                 if (snapshot.hasData) {
                   // print(snapshot.data!.model);
                   // print(snapshot.data!.price);
-                  return SearchView(image: widget.image,);
-                  // return Center(
-                  //   child: Column(
-                  //     children: [
-                  //       const SizedBox(
-                  //         height: 40,
-                  //       ),
-                  //       Image.file(
-                  //         widget.image,
-                  //         width: 250,
-                  //         height: 250,
-                  //         fit: BoxFit.cover,
-                  //       ),
-                  //       Padding(
-                  //         padding: const EdgeInsets.all(45.0),
-                  //         child: Text(
-                  //           searchImageController.getModel(),
-                  //           // snapshot.data!.model,
-                  //           //"Wagon R Stingray 2018",
-                  //           style: const TextStyle(
-                  //             fontSize: 25,
-                  //             fontWeight: FontWeight.bold,
-                  //             color: Color.fromARGB(255, 59, 77, 85),
-                  //           ),
-                  //         ),
-                  //       ),
-                  //       Padding(
-                  //         padding: const EdgeInsets.all(1.0),
-                  //         child: Text(
-                  //           searchImageController.getPriceFromModel(),
-                  //           // snapshot.data!.price,
-                  //           //"Rs. 6,000,000",
-                  //           style: const TextStyle(
-                  //             fontSize: 25,
-                  //             fontWeight: FontWeight.bold,
-                  //             color: Color.fromARGB(255, 59, 77, 85),
-                  //           ),
-                  //         ),
-                  //       )
-                  //     ],
-                  //   ),
-                  // );
+                  // return SearchView(image: widget.image,);
+                  return Container(
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                        gradient: gd.RadialGradient(
+                            center: Alignment(0, -0.1),
+                            radius: 1,
+                            colors: <Color>[
+                          Color.fromARGB(223, 2, 22, 51),
+                          Color.fromARGB(223, 1, 7, 26),
+                        ])),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        Container(
+                          width: 310,
+                          height: 310,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: FileImage(widget.image),
+                                fit: BoxFit.cover),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 24, 149, 187),
+                              width: 10,
+                            ),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.green,
+                                blurRadius: 10.0,
+                                spreadRadius: 0.0,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        // Container(
+                        //   width: 700,
+                        //   decoration: BoxDecoration(
+                        //     shape: BoxShape.circle,
+                        //     border: Border.all(
+                        //       color: Colors.red,
+                        //       width: 10,
+                        //     )
+                        //   ),
+                        //   child: CircleAvatar(
+                        //     radius: 150,
+                        //     backgroundImage: FileImage(widget.image),
+                        //   ),
+                        // ),
+                        Padding(
+                          padding: const EdgeInsets.all(45.0),
+                          child: Text(
+                            searchImageController.getModel(),
+                            // snapshot.data!.model,
+                            //"Wagon R Stingray 2018",
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 59, 77, 85),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Text(
+                            searchImageController.getPriceFromModel(),
+                            // snapshot.data!.price,
+                            //"Rs. 6,000,000",
+                            style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 59, 77, 85),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
                 } else if (snapshot.hasError) {
                   return Text("${snapshot.error}");
                 } else {
@@ -131,31 +172,49 @@ class _SearchViewState extends State<SearchView> {
             Color.fromARGB(223, 2, 22, 51),
             Color.fromARGB(223, 1, 7, 26),
           ])),
-      child: Stack(alignment: Alignment.center, children: [
-        Container(
-          width: 420,
-          // width: 380,
-          child: const RiveAnimation.asset(
-              'assets/ui_elements/search_loader_breathing.riv'),
-        ),
-        Container(
-          width: 241,
-          // width: 380,
-          child:
-              const RiveAnimation.asset('assets/ui_elements/search_loader.riv'),
-        ),
-        Container(
-          width: 210,
-          height: 210,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-                image: FileImage(widget.image), fit: BoxFit.cover),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 60,
+            width: 60,
           ),
-        ),
-        // Text('Beta version could take upto 40 seconds due to server limitations.')
-        Text('Beta version.')
-      ]),
+          Stack(alignment: Alignment.center, children: [
+            const SizedBox(
+              height: 520,
+              width: 450,
+              // width: 380,
+              child: RiveAnimation.asset(
+                  'assets/ui_elements/search_loader_breathing.riv'),
+            ),
+            const SizedBox(
+              height: 241,
+              width: 241,
+              // width: 380,
+              child:
+                  RiveAnimation.asset('assets/ui_elements/search_loader.riv'),
+            ),
+            Container(
+              width: 210,
+              height: 210,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: FileImage(widget.image), fit: BoxFit.cover),
+              ),
+            ),
+          ]),
+          const SizedBox(
+            height: 50,
+            width: 50,
+          ),
+          const Text('Beta version could take up to 40 seconds due to server limitations.',
+          style: TextStyle(
+            color: Color.fromARGB(157, 173, 212, 248),
+            fontSize: 12,
+            fontStyle: FontStyle.italic,
+          ),)
+        ], 
+      ),
     );
   }
 }
