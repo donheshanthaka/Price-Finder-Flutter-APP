4879f8c8c13fb88cab2c15e98c326c592e255712
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-17 14:12:05 +0530

    Code refactoring: main_common.dart, main_dev.dart and main_prod.dart

--- lib/main/main_common.dart
+++ lib/main/main_common.dart
@@ -1,16 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:price_finder/utils/config_reader.dart';
 import 'package:price_finder/main/app.dart';
 
-
 Future<void> mainCommon(String env) async {
   WidgetsFlutterBinding.ensureInitialized();
   await ConfigReader.initialize(env);
 
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
-    .then((_) {
-      runApp(const MyApp());
-    });
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(const MyApp());
+  });
 }
