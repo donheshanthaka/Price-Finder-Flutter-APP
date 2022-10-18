a359306ccd015cc595016caa4040a45173656e81
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-19 00:15:20 +0530

    Code refactoring: image_model.dart

--- lib/features/select_image/models/image_model.dart
+++ lib/features/select_image/models/image_model.dart
@@ -2,26 +2,22 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:price_finder/utils/failure.dart';
 
 class ImageModel {
   static Future<File?> getImage(String imageSource) async {
     late ImageSource source;
-    if (imageSource == 'camera'){
+    if (imageSource == 'camera') {
       source = ImageSource.camera;
-    } else if (imageSource == 'gallery'){
+    } else if (imageSource == 'gallery') {
       source = ImageSource.gallery;
     }
     try {
       final image = await ImagePicker().pickImage(source: source);
       if (image == null) return null;
-      //final savedImage = await _saveImage(image.path);
-
-      //await predictImage(imageSaved);
-      //return savedImage;
       return File(image.path);
     } on PlatformException catch (e) {
-      print('Failed to display image: $e');
+      throw Failure('Failed to capture image: $e');
     }
-    return null;
   }
 }
