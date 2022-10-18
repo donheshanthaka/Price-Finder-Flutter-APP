db2eeaa55c4bc3c7034b6f83f67da79ec1259d32
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-19 03:47:47 +0530

    Code refactoring: search_image_controller.dart

--- lib/features/search_image/controller/search_image_controller.dart
+++ lib/features/search_image/controller/search_image_controller.dart
@@ -16,62 +16,12 @@ import 'package:price_finder/common_screens/error_screen.dart';
 class SearchImageController extends ControllerMVC {
   factory SearchImageController([StateMVC? state]) =>
       _this ??= SearchImageController._(state);
-  SearchImageController._(StateMVC? state): super(state);
+  SearchImageController._(StateMVC? state) : super(state);
   static SearchImageController? _this;
-  
+
   Vehicle vehicle = Vehicle();
 
-  Future<List<String>> predictImage(File file) async {
-    //String url = "https://get-prediction-hezxtblxwq-el.a.run.app";
-    String localPredUrl =
-        "http://192.168.1.101:8000/"; // // use the current ip address by running ipconfig at the time of debugging using emulator
-
-    //print('Image path >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-    //print(file.path);
-
-    List<String> info = [];
-
-    var request = http.MultipartRequest("POST", Uri.parse(localPredUrl));
-
-    var multipartFile =
-        await http.MultipartFile.fromPath('imageFile', file.path);
-
-    request.files.add(multipartFile);
-
-    try {
-      var response = await request.send().timeout(const Duration(seconds: 10));
-      if (response.statusCode == 200) {
-        var responseData = await response.stream.toBytes();
-        var vehicleName = String.fromCharCodes(responseData);
-        info.add(vehicleName);
-        info.add(await getPrice(vehicleName));
-        return info;
-      } else {
-        throw Exception("Error: Falied to indentify image");
-      }
-    } on SocketException {
-      throw Failure("Error: Cannot connect to server!");
-    } on TimeoutException {
-      throw Failure("Error: Server timeout!");
-    }
-  }
-
-  Future<String> getPrice(String vehicleName) async {
-    Map<String, String> params = {"vehicle": vehicleName};
-    String localPriceUrl =
-        "http://192.168.1.101:8000/price"; // use the current ip address by running ipconfig at the time of debugging using emulator
-    Uri url = Uri.parse(localPriceUrl);
-    url = url.replace(queryParameters: params);
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception("Failed to get price");
-    }
-  }
-
   Future<bool> getVehicleInfo(File image) async {
-    // String url = '${ConfigReader.getApiUrl()}/test';
     String url = ConfigReader.getApiUrl();
     var request = http.MultipartRequest("POST", Uri.parse(url));
     var multipartFile =
@@ -82,7 +32,8 @@ class SearchImageController extends ControllerMVC {
       var streamedResponse =
           await request.send().timeout(const Duration(seconds: 60));
       var response = await http.Response.fromStream(streamedResponse);
-      final result = jsonDecode(response.body.toString()) as Map<String, dynamic>;
+      final result =
+          jsonDecode(response.body.toString()) as Map<String, dynamic>;
       if (streamedResponse.statusCode == 200) {
         vehicle.updateInfo(result);
         return true;
@@ -99,25 +50,25 @@ class SearchImageController extends ControllerMVC {
     }
   }
 
-  String getModel(){
+  String getModel() {
     return vehicle.getModel();
   }
 
-  String getPriceFromModel(){
+  String getPriceFromModel() {
     return vehicle.getPrice();
   }
 
-  loadVehiclePage(File image){
+  loadVehiclePage(File image) {
     final context = GlobalContextService.navigatorKey.currentContext!;
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => VehiclePage(image: image)));
-   }
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => VehiclePage(image: image)));
+  }
 
   Widget loadSearchView(File image) {
     return SearchView(image: image);
   }
 
-  Widget loadErrorScreen(String error){
+  Widget loadErrorScreen(String error) {
     return ErrorScreen(error: error);
   }
-  
 }
