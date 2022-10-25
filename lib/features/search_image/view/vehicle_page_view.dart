ae3f75c12d8d9bba3fc3306e428c419b7ea3c5f4
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-25 11:20:47 +0530

    Implement responsive gradient value in vehicle page

--- lib/features/search_image/view/vehicle_page_view.dart
+++ lib/features/search_image/view/vehicle_page_view.dart
@@ -49,11 +49,11 @@ class _VehiclePageState extends State<VehiclePage> {
                     height: ScreenUtil().screenHeight,
                     width: ScreenUtil().screenWidth,
                     alignment: Alignment.center,
-                    decoration: const BoxDecoration(
+                    decoration: BoxDecoration(
                       gradient: gd.RadialGradient(
-                        center: Alignment(0, -0.1),
-                        radius: 1,
-                        colors: <Color>[
+                        center: const Alignment(0, -0.1),
+                        radius: 3.7.r,
+                        colors: const <Color>[
                           Color.fromARGB(223, 2, 22, 51),
                           Color.fromARGB(223, 1, 7, 26),
                         ],
