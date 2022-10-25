bedd0ba3d2cf12cf4c75b363b236e199d93defa6
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-25 10:56:33 +0530

    Implement responsive UI design in splash screen

--- lib/common_screens/splash_screen.dart
+++ lib/common_screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:flutter/src/painting/gradient.dart' as gd;
 import 'package:price_finder/common_screens/home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -28,21 +29,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Color.fromARGB(255, 26, 39, 56),
       body: Container(
-        decoration: const BoxDecoration(
-            gradient: gd.RadialGradient(
-                center: Alignment(0, -0.1),
-                radius: 1,
-                colors: <Color>[
+        height: ScreenUtil().screenHeight,
+        width: ScreenUtil().screenWidth,
+        decoration: BoxDecoration(
+          gradient: gd.RadialGradient(
+            center: const Alignment(0, -0.1),
+            radius: 3.7.r,
+            colors: const <Color>[
               Color.fromARGB(223, 2, 22, 51),
               Color.fromARGB(223, 1, 7, 26),
-            ])),
-        child: const Center(
+            ],
+          ),
+        ),
+        child: Center(
           child: SizedBox(
-            width: 400,
-            child:
-                RiveAnimation.asset('assets/splash_screen/splash_screen.riv'),
+            width: ScreenUtil().setWidth(1400),
+            child: const RiveAnimation.asset(
+                'assets/splash_screen/splash_screen.riv'),
           ),
         ),
       ),
