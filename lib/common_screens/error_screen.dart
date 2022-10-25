f52a12e51b2402bf2dbe5f2d12382fcba1c64303
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-25 11:23:45 +0530

    Code refactor: error_screen.dart

--- lib/common_screens/error_screen.dart
+++ lib/common_screens/error_screen.dart
@@ -19,7 +19,6 @@ class _ErrorScreenState extends State<ErrorScreen> {
       decoration: BoxDecoration(
         gradient: RadialGradient(
           center: const Alignment(0, -0.1),
-          // radius: 1,
           radius: 3.7.r,
           colors: const <Color>[
             Color.fromARGB(223, 2, 22, 51),
@@ -30,29 +29,23 @@ class _ErrorScreenState extends State<ErrorScreen> {
       child: Column(
         children: [
           SizedBox(
-            // height: 125,
             height: ScreenUtil().setHeight(462.5),
           ),
           SizedBox(
-            // height: 250,
-            // width: 250,
             height: ScreenUtil().setHeight(928),
             width: ScreenUtil().setWidth(928),
             child: Image.asset(
                 'assets/ui_elements/error_icons/broken_search_icon.png'),
           ),
           SizedBox(
-            // height: 55,
             height: ScreenUtil().setHeight(210),
           ),
           Padding(
-            // padding: const EdgeInsets.all(20.0),
             padding: EdgeInsets.all(70.h),
             child: Text(
               widget.error,
               style: TextStyle(
                 color: const Color.fromARGB(157, 173, 212, 248),
-                // fontSize: 18,
                 fontSize: 67.sp,
                 fontStyle: FontStyle.italic,
               ),
