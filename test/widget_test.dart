7031b8cd6c0d6718a8121715701b272981430fe0
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-28 23:11:25 +0530

    Implement widget test for home page search button

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,41 +1,30 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:price_finder/common_screens/home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
-
-  testWidgets("Test search button", (WidgetTester tester) async {
-    final searchButton = find.byKey(const ValueKey("SearchButton"));
-    await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
-    await tester.pump();
-    await tester.tap(searchButton);
-    await tester.pump();
-    expect(find.byType(CupertinoAlertDialog), findsOneWidget);
-
-  });
+  testWidgets(
+    "Test search button",
+    (WidgetTester tester) async {
+      final searchButton = find.byKey(const ValueKey("SearchButton"));
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ScreenUtilInit(
+            designSize: const Size(1440, 2960),
+            builder: (context, widget) {
+              return const MaterialApp(
+                home: HomeScreen(),
+              );
+            },
+          ),
+        ),
+      );
+      await tester.pump();
+      await tester.tap(searchButton);
+      await tester.pump();
+      expect(find.byType(CupertinoAlertDialog), findsOneWidget);
+    },
+  );
 }
